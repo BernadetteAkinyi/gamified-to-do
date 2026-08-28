d3cef39048265c1416ec128dd234036be7ef9d2a
Bernadette Akinyi <bernadetteodongo@gmail.com>
2026-08-28 14:00:07 +0300

    Call task handler

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -7,7 +7,9 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/tasks", api.HomePage)
+
+	http.HandleFunc("/", api.HomePage)
+	http.HandleFunc("/tasks", api.AddTask)
 
 	fmt.Println("http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
